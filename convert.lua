dc77c4cbf8d504909f96a0bd09caa90029ed8706
hugeblank <toddlangejr@gmail.com>
2018-11-28 01:31:15 -0800

    Tweaks to core functionality
    - previous change ".hvfs files rewrite themselves so that their contents are saved on exit" now works as intended, rather than only twice
    - changed the convert function back to its former state, returns the tree table instead of an executable string
    - What convert previously returned has been moved to the new genExec function, which takes in a virtual filesystem and emits an executable chunk
    
    recursive strings aren't friendly in lua...

--- convert.lua
+++ convert.lua
@@ -1,9 +1,9 @@
 local args = {...}
-local convert = require("harbor").convert
+local genExec = require("harbor").genExec
 if fs.exists(args[1]) then
     if fs.isDir(args[1]) then
         local file = fs.open(fs.getName(args[1])..".hvfs", "w")
-        file.write(convert(args[1]))
+        file.write(genExec(args[1]))
         file.close()
         print(fs.combine(args[1], "").." converted")
     else
